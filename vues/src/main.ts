9eaf86e34a4e3b2d4957ec2b89b99efd727d9320
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-04-17 21:52:06 -0400

    real firebase

--- vues/src/main.ts
+++ vues/src/main.ts
@@ -12,3 +12,21 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBa2djPq0QhVTxpiWNxKz3JmmIG0YZDJZ0",
+
+  authDomain: "blah-23b05.firebaseapp.com",
+
+  projectId: "blah-23b05",
+
+  storageBucket: "blah-23b05.appspot.com",
+
+  messagingSenderId: "707148373267",
+
+  appId: "1:707148373267:web:102bfd1c47112b36870ee6",
+
+  measurementId: "G-C3RWTQ76JG"
+})
+const auth = getAuth(firebaseApp);
