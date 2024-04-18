641998a6561532cb420721bc0ba480fd49f52164
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-04-18 10:14:00 -0400

    auth

--- vues/src/main.ts
+++ vues/src/main.ts
@@ -1,32 +1,30 @@
-import './assets/main.css'
+import { initializeApp } from 'firebase/app';
+import {
+  connectAuthEmulator,
+  getAuth,
+  onAuthStateChanged,
+  signInWithCustomToken,
+  signOut,
+} from 'firebase/auth';
+import { firebaseConfig } from './config';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+initializeApp(firebaseConfig);
+const auth = getAuth();
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-import App from './App.vue'
-import router from './router'
+const firebaseConfig = {
+    apiKey: "AIzaSyBa2djPq0QhVTxpiWNxKz3JmmIG0YZDJZ0",
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBa2djPq0QhVTxpiWNxKz3JmmIG0YZDJZ0",
-
-  authDomain: "blah-23b05.firebaseapp.com",
-
-  projectId: "blah-23b05",
-
-  storageBucket: "blah-23b05.appspot.com",
-
-  messagingSenderId: "707148373267",
-
-  appId: "1:707148373267:web:102bfd1c47112b36870ee6",
-
-  measurementId: "G-C3RWTQ76JG"
-})
-const auth = getAuth(firebaseApp);
+    authDomain: "blah-23b05.firebaseapp.com",
+  
+    projectId: "blah-23b05",
+  
+    storageBucket: "blah-23b05.appspot.com",
+  
+    messagingSenderId: "707148373267",
+  
+    appId: "1:707148373267:web:102bfd1c47112b36870ee6",
+  
+    measurementId: "G-C3RWTQ76JG"
+};
