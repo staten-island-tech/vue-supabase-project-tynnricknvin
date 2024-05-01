ba5b066a18d932f2f930a213d64eaaa98e10a774
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-05-01 10:10:10 -0400

    so hard

---
+++ vues/src/views/InView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="prestige">
+  <h3>Your Clicks: {{ clicked }}</h3>
+  <h2>Prestige</h2>
+  <button @click="prestige"> Click Me</button> 
+  <h2>PP gain: {{ prestige_gain }}</h2>
+  <h3>Cost: 1000000</h3>
+  </div>
+</template>
+<script setup <template>
+  <div class="prestige">
+  <h3>Your Clicks: {{ clicked }}</h3>
+  <h2>Prestige</h2>
+  <button @click="prestige"> Click Me</button> 
+  <h2>PP gain: {{ prestige_gain }}</h2>
+  <h3>Cost: 1000000</h3>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
+>
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
