2bd6614f729ee5c3bee1431503b099227c7346a5
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-05-10 09:36:17 -0400

    the better vue setup

---
+++ vues/src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+      <input
+    class="mdl-textfield__input"
+    style="display: inline; width: auto"
+    type="text"
+    id="email"
+    name="email"
+    placeholder="Email"
+  />
+
+</template>
+  
+  <script setup lang="ts">
+
+    </script>
+  
+  <style scoped>
+  .pet-img {
+  width: 50px; 
+  height: 50px; 
+  object-fit: cover;
+  }
+  img {
+    width: 250px;
+    height: 300px;
+    object-fit: cover;
+  }
+  </style>
---
+++ vues/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <DestCard/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DestCard from "@/components/HelloWorld.vue";
+</script>
+
+<style scoped>
+
+</style>
